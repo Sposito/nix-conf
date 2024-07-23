564f65fc133985ea33c0d5f959b9219df1077a37
Thiago <sposito.thiago@gmail.com>
2024-07-23 11:32:24 -0300

    update

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -36,7 +36,11 @@
     config = {
       allowUnfree = true;
       # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = _: true;
+      nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+        "steam"
+        "steam-original"
+        "steam-run"
+      ];
     };
   };
 
