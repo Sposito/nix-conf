e7722a7db93fd0e08798ade02fd4aac56f1ac3ad
Thiago <sposito.thiago@gmail.com>
2024-07-24 19:34:12 -0300

    update

--- hosts/Nixstation/default.nix
+++ hosts/Nixstation/default.nix
@@ -8,23 +8,43 @@
   imports = [
     ./hardware-configuration.nix
     ../common/default.nix
+    ../common/network.nix
     ../common/screen.nix
     ../common/nvidia/default.nix
     ../common/podman/default.nix
   ];
 
-  # Bootloader.
+  nixpkgs.config.allowUnfree = true;
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  
-  networking.hostName = "Nixstation";
 
+  time.timeZone = "America/Sao_Paulo";
+
+  services.xserver.xkb = {  # Configure keymap in X11
+    layout = "us";
+    variant = "alt-intl";
+  };
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
+  };
+  
+  
   environment.systemPackages = with pkgs; [
     wget
     python3
     git
     exfat
-    neovim
     nil
     nixpkgs-fmt
     home-manager
@@ -44,35 +64,17 @@
      # Open ports in the firewall for Steam Local Network Game Transfers
   };
 
-  networking.networkmanager.enable = true;
-  time.timeZone = "America/Sao_Paulo";
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pt_BR.UTF-8";
-    LC_IDENTIFICATION = "pt_BR.UTF-8";
-    LC_MEASUREMENT = "pt_BR.UTF-8";
-    LC_MONETARY = "pt_BR.UTF-8";
-    LC_NAME = "pt_BR.UTF-8";
-    LC_NUMERIC = "pt_BR.UTF-8";
-    LC_PAPER = "pt_BR.UTF-8";
-    LC_TELEPHONE = "pt_BR.UTF-8";
-    LC_TIME = "pt_BR.UTF-8";
-  };
-
-  # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  services.samba.enable = true;
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "thiago";
+  
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "alt-intl";
-  };
+
 
 
   # Enable CUPS to print documents.
@@ -87,16 +89,11 @@
     pulse.enable = true;
   };
 
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = "thiago";
 
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
 
-  nixpkgs.config.allowUnfree = true;
 
-  services.openssh.enable = true;
+
+    
   virtualisation.spiceUSBRedirection.enable = true;
   virtualisation.libvirtd = {
     enable = true;
