78105a36d372102a9e2aee33e8b0588cff6a842d
Thiago <sposito.thiago@gmail.com>
2024-07-28 15:53:54 -0300

    kitty

---
+++ home-manager/kitty.nix
@@ -0,0 +1,21 @@
+{ inputs
+, lib
+, config
+, pkgs
+, ...
+}: {
+    
+    programs.kitty = {
+      enable = true;
+      theme = "Nord";
+      settings = {
+        
+        hide_window_decorations = "titlebar-only";
+        font_size = 16.0;
+        inactive_text_alpha = 0.6;
+        macos_thicken_font = 0.75;
+        background_blur = 1;
+      };
+    };
+}
+
