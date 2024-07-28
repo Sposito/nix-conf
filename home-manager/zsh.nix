e7fa045d7d1098ae12b51245c78b6907b849533b
Thiago <sposito.thiago@gmail.com>
2024-07-28 12:56:49 -0300

    update

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -1,13 +1,13 @@
 { inputs
-, lib
-, config
-, pkgs
+# , lib
+# , config
+# , pkgs
 , ...
 }: {
     
  programs.zsh = {
     enable = true;
-    programs.zsh.autosuggestion.enable = true;
+    autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     oh-my-zsh = {
         enable = true;
