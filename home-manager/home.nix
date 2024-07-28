6469e2152f5b0b4d086fdfbbc8692c9f994d879e
Thiago <sposito.thiago@gmail.com>
2024-07-28 12:31:22 -0300

    update

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -82,26 +82,26 @@
       userName = "Thiago Sposito";
     };
 
-  #   vscode = {
-  #     enable = true;
-  #     package = pkgs.vscode;
-  #     extensions = with pkgs.vscode-extensions; [
-  #       eamodio.gitlens
-  #       bbenoist.nix
-  #       jnoortheen.nix-ide
-  #       arcticicestudio.nord-visual-studio-code
-  #     ];
+    # vscode = {
+    #   enable = true;
+    #   package = pkgs.vscode;
+    #   extensions = with pkgs.vscode-extensions; [
+    #     eamodio.gitlens
+    #     bbenoist.nix
+    #     jnoortheen.nix-ide
+    #     arcticicestudio.nord-visual-studio-code
+    #   ];
 
-  #     userSettings = {
-  #       "user.colorTheme" = "Nord";
-  #       "workbench.colorTheme" = "Nord";
-  #       "terminal.integrated.fontFamily" = "Hack";
-  #       "nix.enableLanguageServer" = true;
-  #     };
-  #   };
+    #   userSettings = {
+    #     "user.colorTheme" = "Nord";
+    #     "workbench.colorTheme" = "Nord";
+    #     "terminal.integrated.fontFamily" = "Hack";
+    #     "nix.enableLanguageServer" = true;
+    #   };
+    # };
 
-  #   home-manager.enable = true;
-  # };
+   home-manager.enable = true;
+   };
 
   systemd.user.startServices = "sd-switch";   # Nicely reload system units when changing configs
   home.stateVersion = "24.05";
