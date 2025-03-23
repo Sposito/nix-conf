5baac8281e99712b39a0764efe4f94aa198163be
Thiago Sposito <sposito.thiago@gmail.com>
2025-03-23 12:46:20 -0300

    feat: Add statix config file

--- hosts/Nixstation/hardware-configuration.nix
+++ hosts/Nixstation/hardware-configuration.nix
@@ -2,7 +2,7 @@
 # and may be overwritten by future invocations.  Please make changes
 # to /etc/nixos/configuration.nix instead.
 # orig
-{ config, lib, pkgs, modulesPath, ... }:
+{ config, lib, modulesPath, ... }:
 
 {
   imports =
