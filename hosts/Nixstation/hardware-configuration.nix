bcd29bf430cd306b9cd8923923901f84569fb0f0
Thiago Sposito <sposito.thiago@gmail.com>
2025-03-24 18:51:14 -0300

    optimize system

--- hosts/Nixstation/hardware-configuration.nix
+++ hosts/Nixstation/hardware-configuration.nix
@@ -2,46 +2,54 @@
 # and may be overwritten by future invocations.  Please make changes
 # to /etc/nixos/configuration.nix instead.
 # orig
-{ config, lib, modulesPath, ... }:
+{
+  config,
+  lib,
+  modulesPath,
+  ...
+}:
 
 {
-  imports =
-    [
-      (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
   boot = {
-    initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+    initrd.availableKernelModules = [
+      "xhci_pci"
+      "ehci_pci"
+      "nvme"
+      "usbhid"
+      "usb_storage"
+      "sd_mod"
+    ];
     initrd.kernelModules = [ ];
     kernelModules = [ "kvm-intel" ];
     extraModulePackages = [ ];
   };
 
-
   fileSystems = {
-    "/" =
-      {
-        device = "/dev/disk/by-uuid/df74093a-637d-41a5-8c6a-2bf2dccc1506";
-        fsType = "btrfs";
-        options = [ "subvol=@" ];
-      };
+    "/" = {
+      device = "/dev/disk/by-uuid/df74093a-637d-41a5-8c6a-2bf2dccc1506";
+      fsType = "btrfs";
+      options = [ "subvol=@" ];
+    };
 
-    "/boot" =
-      {
-        device = "/dev/disk/by-uuid/E9BA-D1A3";
-        fsType = "vfat";
-        options = [ "fmask=0022" "dmask=0022" ];
-      };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/E9BA-D1A3";
+      fsType = "vfat";
+      options = [
+        "fmask=0022"
+        "dmask=0022"
+      ];
+    };
 
-    "/mnt/hdd0" =
-      {
-        device = "/dev/disk/by-uuid/940353dd-5774-4577-aba3-516d3f9c404d";
-        fsType = "btrfs";
-        options = [ "defaults" ];
-      };
+    "/mnt/hdd0" = {
+      device = "/dev/disk/by-uuid/940353dd-5774-4577-aba3-516d3f9c404d";
+      fsType = "btrfs";
+      options = [ "defaults" ];
+    };
   };
-
-
 
   swapDevices = [ ];
 
