fb1c490e915dbae54f552fd1a3fd32108de609a6
Thiago Sposito <sposito.thiago@gmail.com>
2025-04-10 20:05:11 -0300

    test host

---
+++ hosts/Nixtest/scripts/install_test_shell.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  name = "nixos-anywhere-test-env";
+
+  packages = with pkgs; [
+    nix
+    nixos-anywhere
+    openssh
+    sshpass
+    qemu_full
+    wget
+  ];
+
+  shellHook = ''
+    echo "Entered NixOS Anywhere Test Environment."
+    echo "Alpine image will be downloaded if needed."
+    echo "Run ./install_test.sh to start the QEMU VM and run the installation."
+  '';
+}
