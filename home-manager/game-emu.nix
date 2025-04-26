26a29ba5f97341350bf5cdeeb98584767d7416c6
Thiago Sposito <sposito.thiago@gmail.com>
2025-04-26 11:18:02 -0300

    add switch emulator

---
+++ home-manager/game-emu.nix
@@ -0,0 +1,14 @@
+{ inputs, lib, ... }:
+
+let
+  nixpkgs-unstable = import inputs.nixpkgs-unstable {
+    system = "x86_64-linux";
+    config.allowUnfree = true;
+    config.allowUnfreePredicate = pkg:
+      builtins.elem (lib.getName pkg) [];
+  };
+in {
+  home.packages = with nixpkgs-unstable; [
+    torzu
+  ];
+}
