925422a94f125df1f80ba8fa26cc432fcf632abf
Thiago Sposito <sposito.thiago@gmail.com>
2025-07-20 18:41:19 -0300

    fmt fix

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
 
     vscode-extensions = {
       url = "github:nix-community/nix-vscode-extensions";
-      inputs.nixpkgs.follows = "nixpkgs"; 
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     flake-utils.follows = "vscode-extensions/flake-utils";
