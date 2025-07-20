b605fd430aaf46479d1f3c249a5b8614ca9eaa38
Thiago Sposito <sposito.thiago@gmail.com>
2025-07-20 19:40:02 -0300

    chore: improve ci

--- hosts/Nixbook/hardware-configuration.nix
+++ hosts/Nixbook/hardware-configuration.nix
@@ -6,9 +6,6 @@
 , ...
 }:
 
-let
-  machineKey = inputs.machine-key.packages.${pkgs.system}.default;
-in
 {
   imports = [
     (modulesPath + "/hardware/network/broadcom-43xx.nix")
@@ -32,24 +29,4 @@ in
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  environment.systemPackages = [
-    machineKey
-  ];
-
-  # Ensure secure permissions on output file
-  systemd.tmpfiles.rules = [
-    "f /etc/machine_key.bin 0400 root root - -"
-  ];
-
-  # Run the script once at boot
-  systemd.services.generate-machine-key = {
-    description = "Generate machine-specific private key";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${machineKey}/bin/generate-key.sh /etc/machine_key.bin";
-      RemainAfterExit = true;
-    };
-  };
 }
